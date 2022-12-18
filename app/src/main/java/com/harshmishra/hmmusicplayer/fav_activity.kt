86e352a7ab5a01e5d4a9c64ad25112a5757cf252
ShashankShinde16 <ss9575a@gmail.com>
2022-12-18 17:19:13 +0530

    back button (shashank)
    working back button in favourite and playlist

--- app/src/main/java/com/harshmishra/hmmusicplayer/fav_activity.kt
+++ app/src/main/java/com/harshmishra/hmmusicplayer/fav_activity.kt
@@ -1,5 +1,6 @@
 package com.harshmishra.hmmusicplayer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.harshmishra.hmmusicplayer.databinding.ActivityFavBinding
@@ -11,5 +12,11 @@ class fav_activity : AppCompatActivity() {
         setTheme(R.style.coolPink)
         binding = ActivityFavBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //        back button working
+        binding.backBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/harshmishra/hmmusicplayer/playlist_activtiy.kt
+++ app/src/main/java/com/harshmishra/hmmusicplayer/playlist_activtiy.kt
@@ -1,5 +1,6 @@
 package com.harshmishra.hmmusicplayer
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.harshmishra.hmmusicplayer.databinding.ActivityPlaylistActivtiyBinding
@@ -13,5 +14,11 @@ class playlist_activtiy : AppCompatActivity() {
         setTheme(R.style.coolPink)
         binding = ActivityPlaylistActivtiyBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //        back button working
+        binding.backBtn1.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
